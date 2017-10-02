0ece82d5d5e58c4a4235d3497061dc502f9793e5
haxsgvnbdus <haxsgvnbdus@ADMINs-MacBook-Pro-2.local>
2017-10-02 13:39:57 -0600

    3D house scene

---
+++ README.c
@@ -0,0 +1,21 @@
+
+Name: Han Ngo
+Simple 3D Drawing
+Simple 3D Visualization of houses 
+
+BUILDING (OSX):
+1. To compile, in terminal, type 'make'
+2. To run, type './hw'
+
+Screenshot of the program as attached.
+Time to complete the assignment: 6-7hrs
+ 
+Key bindings:
+   a          Toggle axes
+   arrows     Change view angle
+   0          Reset view angle
+   ESC        Exit
+
+Time to complete the assignment: 6-7hrs
+ 
+ 
